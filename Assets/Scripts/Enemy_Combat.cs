b0abb50a04cd62b282d2c1ad8e7ce2f86bcfdeec
Anton Grebinnyi <grants404@outlook.com>
2024-10-01 15:03:21 +0800

    Added knockback for the enemy attack
    Enemy attacks have knockback effect and the player can dodge them.

--- Assets/Scripts/Enemy_Combat.cs
+++ Assets/Scripts/Enemy_Combat.cs
@@ -7,15 +7,9 @@ public class Enemy_Combat : MonoBehaviour
     public int damage = 1;
     public Transform attackPoint;
     public float weaponRange;
+    public float knockbackForce;
+    public float stunTime;
     public LayerMask playerLayer;
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
-        }   
-    }
 
     public void Attack()
     {
@@ -24,6 +18,7 @@ public class Enemy_Combat : MonoBehaviour
         if (hits.Length > 0)
         {
             hits[0].GetComponent<PlayerHealth>().ChangeHealth(-damage);
+            hits[0].GetComponent<PlayerMovement>().Knockback(transform, knockbackForce, stunTime);
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,22 +10,27 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator anim;
 
+    private bool isKnockedBack;
+
     // FixedUpdate is called 50 times per second
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        if (horizontal > 0 && transform.localScale.x < 0 ||
-            horizontal < 0 && transform.localScale.x > 0)
+        if (isKnockedBack == false)
         {
-            Flip();
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            if (horizontal > 0 && transform.localScale.x < 0 ||
+                horizontal < 0 && transform.localScale.x > 0)
+            {
+                Flip();
+            }
+
+            anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+            anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+            rb.velocity = new Vector2 (horizontal, vertical) * speed;
         }
-
-        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
-        anim.SetFloat("vertical", Mathf.Abs(vertical));
-
-        rb.velocity = new Vector2 (horizontal, vertical) * speed;
     }
 
     void Flip()
@@ -33,5 +38,20 @@ public class PlayerMovement : MonoBehaviour
         facingDirection *= -1;
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
 
+    }
+
+    public void Knockback(Transform enemy, float force, float stunTime)
+    {
+        isKnockedBack = true;
+        Vector2 direction = (transform.position - enemy.position).normalized;
+        rb.velocity = direction * force;
+        StartCoroutine(KnockbackCounter(stunTime));
+    }
+
+    IEnumerator KnockbackCounter(float stunTime)
+    {
+        yield return new WaitForSeconds(stunTime);
+        rb.velocity = Vector2.zero;
+        isKnockedBack = false;
     }
 }
