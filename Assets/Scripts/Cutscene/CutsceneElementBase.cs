86be5763bcb362bd96b002b892763cdb1a92629e
Anton Grebinnyi <grants404@outlook.com>
2024-10-05 15:44:01 +0800

    Setting up the cutscene system
    Includes classes: CutsceneElementBase, CutsceneHandler, CutsceneInitiator.

---
+++ Assets/Scripts/Cutscene/CutsceneElementBase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneElementBase : MonoBehaviour
+{
+    public float duration;
+    private CutsceneHandler cutsceneHandler;
+
+    private void Start()
+    {
+        cutsceneHandler = GetComponent<CutsceneHandler>();
+    }
+
+    public virtual void Execute()
+    {
+    }
+
+    protected IEnumerator WaitAndAdvance()
+    {
+        yield return new WaitForSeconds(duration);
+        cutsceneHandler.PlayNextElement();
+    }
+}
