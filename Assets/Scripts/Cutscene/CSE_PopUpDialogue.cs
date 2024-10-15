1a29c82673c36c6648af7bc3ebc48a707f0a5366
Anton Grebinnyi <grants404@outlook.com>
2024-10-15 23:37:19 +0800

    Dialogue box
    Basic functionality for a dialogue pop up window such as animations and closing it

---
+++ Assets/Scripts/Cutscene/CSE_PopUpDialogue.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CSE_PopUpDialogue : CutsceneElementBase
+{
+    [SerializeField] private TMP_Text popUpText;
+    [SerializeField] private GameObject dialogueBox;
+    [TextArea] [SerializeField] private string dialogue;
+    [SerializeField] Animator anim;
+
+    private bool isActive;
+
+    public override void Execute()
+    {
+        popUpText.gameObject.SetActive(true);
+        anim.Play("dialogue_box_appear");
+        isActive = true;
+        popUpText.text = dialogue;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && isActive)
+        {
+            anim.Play("dialogue_box_disappear");
+            popUpText.gameObject.SetActive(false);
+
+        }
+    }
+}
