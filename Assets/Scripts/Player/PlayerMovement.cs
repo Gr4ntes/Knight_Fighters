893e58f60cb6caf2dfacd772093c1365c64e9db1
Anton Grebinnyi <grants404@outlook.com>
2024-10-17 15:23:33 +0800

    Disabled player attack together with movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -25,7 +25,7 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetButtonDown("Slash"))
+        if (Input.GetButtonDown("Slash") && !isMovementDisabled)
         {
             player_Combat.Attack();
         }
