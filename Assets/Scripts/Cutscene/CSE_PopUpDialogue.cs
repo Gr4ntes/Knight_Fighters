4e87bc89dc1d1e42d97afb39b17ac00b382f1cc0
Anton Grebinnyi <grants404@outlook.com>
2024-10-17 11:19:48 +0800

    Added typewriter text effect to the dialogues
    The characters are now printed one by one at specified speed.

--- Assets/Scripts/Cutscene/CSE_PopUpDialogue.cs
+++ Assets/Scripts/Cutscene/CSE_PopUpDialogue.cs
@@ -11,23 +11,65 @@ public class CSE_PopUpDialogue : CutsceneElementBase
     [TextArea] [SerializeField] private string dialogue;
     [SerializeField] Animator anim;
 
-    private bool isActive;
+    private bool isActive = false;
+    private bool finishedTyping = false;
+    public float charactersPerSecond = 12;
 
     public override void Execute()
     {
         popUpText.gameObject.SetActive(true);
+        popUpText.text = "";
         anim.Play("dialogue_box_appear");
         isActive = true;
-        popUpText.text = dialogue;
+        StartCoroutine(TypeTextUncapped(dialogue));
+
+    }
+
+    IEnumerator TypeTextUncapped(string line)
+    {
+        float timer = 0;
+        float interval = 1 / charactersPerSecond;
+        string textBuffer = null;
+        char[] chars = line.ToCharArray();
+        int i = 0;
+
+        while (i < chars.Length)
+        {
+            if (timer < Time.deltaTime)
+            {
+                textBuffer += chars[i];
+                popUpText.text = textBuffer;
+                print(textBuffer);
+                timer += interval;
+                i++;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+                yield return null;
+            }
+        }
+        finishedTyping = true;
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Interact") && isActive)
+        if (Input.GetButtonDown("Interact") && isActive && finishedTyping)
         {
             anim.Play("dialogue_box_disappear");
             popUpText.gameObject.SetActive(false);
             cutsceneHandler.PlayNextElement();
         }
+        else if (Input.GetButtonDown("Interact") && isActive && !finishedTyping)
+        {
+            StopAllCoroutines();
+            finishedTyping = true;
+            popUpText.text = dialogue;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
     }
 }
